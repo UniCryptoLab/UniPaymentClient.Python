168c54b2119db8c508c44f63e0b8165da725167b
Shameem Peerbuccoss <speerbuc@gmail.com>
2023-09-04 17:51:11 +0400

    Update Version to 1.1.0

--- UniPaymentClient/unipayment/api_client.py
+++ UniPaymentClient/unipayment/api_client.py
@@ -27,7 +27,7 @@ from .configuration import Configuration
 
 
 # python 2 and python 3 compatibility library
-SDK_VERSION = '1.0.3'
+SDK_VERSION = '1.1.0'
 
 class ApiClient(object):
     PRIMITIVE_TYPES = (float, bool, bytes, six.text_type) + six.integer_types
